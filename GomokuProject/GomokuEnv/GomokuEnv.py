d315f90e98faa844699202e9c0c7c8917b37031e
Yaroslav Linder <yaroslav.linder@gmail.com>
2019-08-29 15:41:35 +0300

    slight change in model arch

--- GomokuProject/GomokuEnv/GomokuEnv.py
+++ GomokuProject/GomokuEnv/GomokuEnv.py
@@ -8,45 +8,7 @@ import sys
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 from itertools import groupby
 import pyglet
-from gym.envs.classic_control.rendering import Viewer
 
-class MyViewer(Viewer):
-    def __init__(self, width, height, display=None):
-        self.labels = []
-        super(MyViewer,self).__init__(width, height, display)
-
-    def add_label(self, label):
-        self.labels.append(label)
-
-    def render(self, return_rgb_array=False):
-        pyglet.gl.glClearColor(1,1,1,1)
-        self.window.clear()
-        self.window.switch_to()
-        self.window.dispatch_events()
-        self.transform.enable()
-        for geom in self.geoms:
-            geom.render()
-        for geom in self.onetime_geoms:
-            geom.render()
-        self.transform.disable()
-        for label in self.labels:
-            label.draw()
-        arr = None
-        if return_rgb_array:
-            buffer = pyglet.image.get_buffer_manager().get_color_buffer()
-            image_data = buffer.get_image_data()
-            arr = np.frombuffer(image_data.data, dtype=np.uint8)
-            # In https://github.com/openai/gym-http-api/issues/2, we
-            # discovered that someone using Xmonad on Arch was having
-            # a window of size 598 x 398, though a 600 x 400 window
-            # was requested. (Guess Xmonad was preserving a pixel for
-            # the boundary.) So we use the buffer height/width rather
-            # than the requested one.
-            arr = arr.reshape(buffer.height, buffer.width, 4)
-            arr = arr[::-1,:,0:3]
-        self.window.flip()
-        self.onetime_geoms = []
-        return arr if return_rgb_array else self.isopen		
 
 class GomokuEnv(MultiAgentEnv):
     win_dim_px=500
@@ -149,6 +111,7 @@ class GomokuEnv(MultiAgentEnv):
 
     def render(self, mode='human'):
         from gym.envs.classic_control import rendering
+        from .MyViewer import MyViewer
         if self.viewer is None:
             self.viewer = MyViewer(GomokuEnv.win_dim_px,GomokuEnv.win_dim_px)
             self.viewer.window.on_mouse_press=self.mouse_press
