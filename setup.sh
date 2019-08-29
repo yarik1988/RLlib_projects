71470c53068a6e225fd5c8c57a188d0a3b6330fe
yarik1988 <yaroslav.linder@gmail.com>
2019-08-29 08:35:00 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-apt install freeglut3-dev
 apt install gnome-core
+apt install freeglut3-dev
 apt install python3-pip
+
+apt install libexo-1-0
+
+pip3 install gym
+pip3 install ray[rllib]
+pip3 install ray[debug]
