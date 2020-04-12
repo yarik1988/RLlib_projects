769339204670685951bd0ce2db66ad7d62ed9e7f
yaroslav <yaroslav.linder@gmail.com>
2020-04-12 12:10:23 +0300

    Update

--- CartpoleProject/CPtrain.py
+++ CartpoleProject/CPtrain.py
@@ -1,12 +1,12 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
 import ray
 import time
+import tensorflow as tf
 import ray.rllib.agents.a3c as a3c
-from ray.rllib.models import ModelCatalog, Model
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.tune.registry import register_env
 import gym
 from gym import RewardWrapper
-from tensorflow.keras import layers, Sequential
 import pickle
 import os
 import _thread
@@ -21,32 +21,35 @@ class ScaleReward(RewardWrapper):
         return reward/100
 
 
-class CartpoleModel(Model):
-    def _build_layers_v2(self, input_dict, num_outputs, options):
-        self.model = Sequential()
-        self.model.add(layers.InputLayer(input_tensor=input_dict["obs"], input_shape=(4,)))
-        self.model.add(layers.Dense(4, name='l1', activation='relu'))
-        self.model.add(layers.Dense(10, name='l2', activation='relu'))
-        self.model.add(layers.Dense(10, name='l3', activation='relu'))
-        self.model.add(layers.Dense(10, name='l4', activation='relu'))
-        self.model.add(layers.Dense(2, name='l5', activation='relu'))
-        return self.model.get_layer("l5").output, self.model.get_layer("l4").output
+class CartpoleModel(TFModelV2):
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        super(CartpoleModel, self).__init__(obs_space, action_space, num_outputs, model_config,name)
+        input_layer = tf.keras.layers.Input(shape=obs_space.shape, name="observations")
+        hidden_layer1 = tf.keras.layers.Dense(15, activation='tanh')(input_layer)
+        hidden_layer2 = tf.keras.layers.Dense(15, activation='tanh')(hidden_layer1)
+        hidden_layer3 = tf.keras.layers.Dense(30, activation='tanh')(hidden_layer2)
+        output_layer = tf.keras.layers.Dense(num_outputs)(hidden_layer3)
+        value_layer = tf.keras.layers.Dense(1)(hidden_layer3)
+        self.base_model = tf.keras.Model(input_layer, [output_layer, value_layer])
+        self.register_variables(self.base_model.variables)
 
+    def forward(self, input_dict, state, seq_lens):
+        model_out, self._value_out = self.base_model(input_dict["obs"])
+        return model_out, state
+
+    def value_function(self):
+        return tf.reshape(self._value_out, [-1])
 
 
 ray.init()
 ModelCatalog.register_custom_model("CartpoleModel", CartpoleModel)
 CartpoleEnv = gym.make('CartPole-v0')
-CartpoleEnv=ScaleReward(CartpoleEnv)
+CartpoleEnv = ScaleReward(CartpoleEnv)
 register_env("CP", lambda _:CartpoleEnv)
 
 
 
-trainer = a3c.A3CTrainer(env="CP", config={
-    #"model": {"custom_model": "CartpoleModel"},
-    #"observation_filter": "MeanStdFilter",
-    #"vf_share_layers": True,
-}, logger_creator=lambda _:ray.tune.logger.NoopLogger({},None))
+trainer = a3c.A3CTrainer(env="CP", config={"model": {"custom_model": "CartpoleModel"}})
 
 if os.path.isfile('weights.pickle'):
    weights = pickle.load(open("weights.pickle", "rb"))
